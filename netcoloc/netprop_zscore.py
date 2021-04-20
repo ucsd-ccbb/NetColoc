dbb048fbd487bef869dd604f81c9aa6609773802
Sara Brin Rosenthal <brin.rosenthal@gmail.com>
2021-04-20 14:48:55 -0700

    notebook updates

--- netcoloc/netprop_zscore.py
+++ netcoloc/netprop_zscore.py
@@ -12,8 +12,10 @@ import os
 from tqdm.auto import tqdm
 
 # Internal module convenience imports
-from .netcoloc_utils import *
-from .netprop import *
+#from .netcoloc_utils import *
+#from .netprop import *
+from netcoloc_utils import *
+from netprop import *
 
 def __init__(self):
     pass
