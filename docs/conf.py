811f0628400cafe0eba55c234f5eb9269ada1463
Chris Churas <churas.camera@gmail.com>
2021-08-31 10:52:22 -0700

    added intersphinx mapping and added link to ddot and python

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,12 @@ import netcoloc
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode',
+              'sphinx.ext.intersphinx']
+
+# intersphinx mapping
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None),
+                       "ddot": ("https://ddot.readthedocs.io/en/latest", None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
