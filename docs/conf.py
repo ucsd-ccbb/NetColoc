65c03cb87de2bd7460e21ee653017804037ca635
Chris Churas <churas.camera@gmail.com>
2021-08-31 12:32:22 -0700

    added numpy to intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,8 @@ extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode',
 intersphinx_mapping = {"python": ("https://docs.python.org/3", None),
                        "ddot": ("https://ddot.readthedocs.io/en/latest", None),
                        "networkx": ("https://networkx.org/documentation/stable", None),
-                       "pandas": ("https://pandas.pydata.org/docs/", None)}
+                       "pandas": ("https://pandas.pydata.org/docs/", None),
+                       "numpy": ("http://docs.scipy.org/doc/numpy", None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
