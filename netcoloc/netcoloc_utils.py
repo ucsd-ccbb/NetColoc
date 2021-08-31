9f8d51e28637f1ffe1d71c1f5ab5ef13253f69bc
Chris Churas <churas.camera@gmail.com>
2021-08-31 13:09:23 -0700

    updated style of documentation

--- netcoloc/netcoloc_utils.py
+++ netcoloc/netcoloc_utils.py
@@ -3,25 +3,32 @@
 '''Utility functions useful across multiple modules.
 '''
 
+
 def get_degree_binning(node_to_degree_dict, min_bin_size, lengths=None):
-    '''
-    Groups nodes by degree into similarly sized bins. This function comes from network_utilities.py of emregtoobox.
-    
-    Args:
-        node_to_degree_dict (dict): Dictionary mapping nodes to their degrees.
-        min_bin_size (int): The minimum number of nodes each bin should contain.
+    """
+    Groups nodes by degree into similarly sized bins. This function
+    comes from
+    `network_utilities.py of emreg00/toolbox <https://github.com/emreg00/toolbox/blob/master/network_utilities.py>`__
+
+
+    Returns a tuple with following two values:
+
+    * **list of bins** where each bin contains a list of nodes of similar degree
+    * **mapping of degree to index of bin** dict mapping a degree to the index
+      of the bin in the bins list which contains nodes of that degree
+
+    :param node_to_degree_dict: Map of nodes to their degrees
+    :type node_to_degree_dict: dict
+    :param min_bin_size: minimum number of nodes each bin should contain.
         lengths (list): List of nodes to bin. If lengths is equal to None, then
             all nodes will be binned.
-
-    Returns:
-        bins (list of lists): A list of bins. Each bin contains a list of nodes
-            of similar degree.
-        degree_to_bin_index (dict): A dictionary mapping a degree to the index 
-            of the bin in the bins list which contains nodes of that degree.
-            For example, in order to retreive the bin containing nodes of degree
-            5: bins[degree_to_bin_index[5]]
-    '''
-    
+    :type min_bin_size: int
+    :param lengths: List of nodes to bin. If lengths is equal to None, then
+                    all nodes will be binned
+    :type lengths: list
+    :return: (list of bins, mapping of degree to index of bin)
+    :rtype: tuple
+    """
     # Create dictionary mapping degrees to nodes
     degree_to_nodes = {}
     for node, degree in node_to_degree_dict.items():
@@ -68,4 +75,3 @@ def get_degree_binning(node_to_degree_dict, min_bin_size, lengths=None):
         degree_index += 1 
 
     return bins, degree_to_bin_index
-    
