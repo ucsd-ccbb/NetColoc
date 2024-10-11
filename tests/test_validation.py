2559132a7bbca1f84454caa9f29eec9828609dd1
sarah-n-wright <sarahwright474@gmail.com>
2024-10-11 16:18:22 -0700

    Added expanded functionality from the cross species BMI project (https://github.com/sarah-n-wright/CrossSpeciesBMI). UNTESTED.

---
+++ tests/test_validation.py
@@ -0,0 +1,33 @@
+
+"""
+test_netcoloc.validation
+----------------------------------
+
+Tests for `validation` module.
+"""
+
+
+import os
+import sys
+import unittest
+import json
+import ndex2
+import networkx as nx
+import pandas as pd
+
+from netcoloc import network_colocalization
+
+
+class TestNetworkColocalization(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        pass
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
