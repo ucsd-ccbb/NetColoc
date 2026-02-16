8fd796244ee9f8f711bf47f6cbd5483fb7c6b79b
sarah-n-wright <sarahwright474@gmail.com>
2026-02-16 11:03:15 -0800

    Bumpversion 1.0.0 > 1.1.0a1

--- netcoloc/__init__.py
+++ netcoloc/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'Brin Rosenthal, Sophie Liu, Sarah Wright'
 __email__ = 'sbrosenthal@health.ucsd.edu, sol015@ucsd.edu, snwright@ucsd.edu'
-__version__ = '1.0.0'
+__version__ = '1.1.0a1'
